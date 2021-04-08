7ee18a57b3fe1ea3663bd7aacd4c22502f945dc3
Lex95 <andrea.lega1995@gmail.com>
2021-04-08 15:42:49 +0200

    aggiunta controlli specifici

--- myscript.js
+++ myscript.js
@@ -3,17 +3,19 @@ var age = parseInt(prompt("Inserire l'età del passeggero:"));
 
 var fee = (kilometers * 0.21);
 
-if (Number.isNaN(kilometers) || Number.isNaN(age)) {
+if ((Number.isNaN(kilometers) || Number.isNaN(age)) || (kilometers <= 0 || age <= 0)) {
     alert("Dati inseriti non validi!");
-}
-else if (age < 18) {
-    var cost = (fee * 0.8).toFixed(2);
-}
-else if (age >= 65) {
-    var cost = (fee * 0.6).toFixed(2);
+    document.getElementById("myId").innerHTML= "Impossibile calcolare la tariffa, ricaricare la pagina.";
 }
 else {
-    var cost = fee.toFixed(2)
+    if (age < 18) {
+        var cost = (fee * 0.8).toFixed(2);
+    }
+    else if (age >= 65) {
+        var cost = (fee * 0.6).toFixed(2);
+    }
+    else {
+        var cost = fee.toFixed(2)
+    }
+    document.getElementById("myId").innerHTML= cost + " €";
 }
-
-document.getElementById("myId").innerHTML= cost + " €";
